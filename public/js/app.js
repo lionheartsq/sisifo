6849504c3b033cde0ed25c7fd5e8cb6457d22938
Carlos Medina <carls.753@gmail.com>
2022-11-24 07:57:30 -0500

    prueba

--- public/js/app.js
+++ public/js/app.js
@@ -110017,8 +110017,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\desarrollo\github\sisifo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\desarrollo\github\sisifo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\sisifo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\sisifo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
