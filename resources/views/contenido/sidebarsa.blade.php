2ae5841bf808f9dc4c53454451b50f48fa404daf
Alexvilla2022 <ealexv@misena.edu.co>
2022-11-27 20:37:37 -0500

    A

--- resources/views/contenido/sidebarsa.blade.php
+++ resources/views/contenido/sidebarsa.blade.php
@@ -80,7 +80,7 @@
                 </ul>
             </li>
             <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-wrench"></i> <b> Reservas</b></a>
+                <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-wrench"></i> <b>Reservas</b></a>
                 <ul class="nav-dropdown-items">
                     <li @click="menu=16" class="nav-item">
                         <a class="nav-link" href="#"><i class="icon-book-open"></i> Lista de Salas</a>
