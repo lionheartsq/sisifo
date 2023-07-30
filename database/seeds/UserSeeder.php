8f53dc8e78f04bc30fda0d38fb56e2e41da48b96
lionheartsq <lujogarpin78@gmail.com>
2023-07-29 21:36:38 -0500

    realignment

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,43 +14,57 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'documento' => '13514998',
+            'name' => 'Paula',
+            'email' => 'pacolmenares02@misena.edu.co',
+            'password' => bcrypt('Paula'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Freddy',
+            'email' => 'freddy.mendez@misena.edu.co',
+            'password' => bcrypt('Instructor'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Luis',
             'email' => 'ljgarcia899@misena.edu.co',
-            'nombres' => 'Luis José',
-            'apellidos' => 'García Pinzón',
-            'password' => bcrypt('12345'),
-            'idEmpresa' => '1',
-            'idRol' => '1',
+            'password' => bcrypt('Luis'),
         ]);
 
         DB::table('users')->insert([
-            'documento' => '12345',
-            'email' => 'empresario@empresario.co',
-            'nombres' => 'Empresario',
-            'apellidos' => 'Prueba',
-            'password' => bcrypt('12345'),
-            'idEmpresa' => '1',
-            'idRol' => '1',
+            'name' => 'Luz',
+            'email' => 'luzariasarias@misena.edu.co',
+            'password' => bcrypt('Instructora'),
         ]);
 
         DB::table('users')->insert([
-            'documento' => '67890',
-            'email' => 'usuario@usuario.co',
-            'nombres' => 'Usuario',
-            'apellidos' => 'Prueba',
-            'password' => bcrypt('12345'),
-            'idEmpresa' => '1',
-            'idRol' => '2',
+            'name' => 'Empresario',
+            'email' => 'empresario@gmail.com',
+            'password' => bcrypt('Empresario'),
         ]);
 
         DB::table('users')->insert([
-            'documento' => '901538355',
-            'email' => 'hotelrioazul@gmail.com',
-            'nombres' => 'Administrador',
-            'apellidos' => 'Hotel',
-            'password' => bcrypt('12345'),
-            'idEmpresa' => '1',
-            'idRol' => '1',
+            'name' => 'Luis',
+            'email' => 'lusuario@misena.edu.co',
+            'password' => bcrypt('Luis'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Empresario',
+            'email' => 'usuario@gmail.com',
+            'password' => bcrypt('Empresario'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Alex E',
+            'email' => 'ealexv@misena.edu.co',
+            'password' => bcrypt('Alex'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Alex U',
+            'email' => 'uealexv@misena.edu.co',
+            'password' => bcrypt('Alex'),
         ]);
 
     }
