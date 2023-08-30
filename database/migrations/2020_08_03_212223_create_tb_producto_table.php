3507ab8f529d92760aa282dd6e830164bd339f49
lionheartsq <lujogarpin78@gmail.com>
2023-08-30 00:25:02 -0500

    Cambios vistas

--- database/migrations/2020_08_03_212223_create_tb_producto_table.php
+++ database/migrations/2020_08_03_212223_create_tb_producto_table.php
@@ -16,13 +16,18 @@ class CreateTbProductoTable extends Migration
         Schema::create('tb_producto', function (Blueprint $table) {
             $table->id();
             $table->string('producto', 255);
-            $table->string('referencia', 255);
+            $table->string('referencia', 255)->nullable();
+            $table->string('plu', 255)->nullable();
             $table->string('foto', 2038);
-            $table->string('descripcion', 255);
+            $table->string('descripcion', 255)->nullable();
+            $table->foreignId('idMedida')->constrained('tb_unidad_base');
             $table->foreignId('idColeccion')->constrained('tb_coleccion');
             $table->foreignId('idArea')->constrained('tb_area');
             $table->foreignId('idEmpresa')->constrained('tb_configuracion_basica');
-            $table->integer('presentacion');
+            $table->foreignId('idImpuesto')->constrained('impuesto');
+            $table->enum('tipo', array('1','2'))->default('2')->comment('1-Term 2-Trans');
+            $table->integer('valorCompra');
+            $table->integer('pvp');
             $table->boolean('estado')->default(1);
             //$table->timestamps();
         });
